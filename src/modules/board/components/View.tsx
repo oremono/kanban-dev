53e8b08fba808b6cf4ece9d8927a71b675bf86c9
oremono <coolrpkohale123@gmail.com>
2025-04-13 19:53:46 +0530

    ref: add useResponsive

--- src/modules/board/components/View.tsx
+++ src/modules/board/components/View.tsx
@@ -2,25 +2,32 @@ import React, { useState } from 'react';
 
 import RenderWeek from './RenderWeek';
 import AnimatedDays from './AnimatedDays';
+import useResponsive from '@hooks/useResponsive';
 
 const View = (props: any) => {
   const { loading, events, eventLen } = props;
+  const { isMobile, isDesktop } = useResponsive();
   const [active, setActive] = useState(0);
 
   return (
     <div className="min-h-screen">
       {loading && <div className="flex justify-center items-center min-h-screen">Loading...</div>}
 
-      {!loading && (
+      {!loading && isMobile && (
         <>
           <div className="p-4 w-full bg-[linear-gradient(to_right,_#3b82f6,_#8b5cf6)]">
             <div className="text-xl font-bold text-white">Your Schedule</div>
 
-            {/* TODO - active */}
             <RenderWeek active={active} setActive={setActive} />
           </div>
 
           <AnimatedDays active={active} setActive={setActive} events={events} eventLen={eventLen} />
+        </>
+      )}
+
+      {!loading && isDesktop && (
+        <>
+          <div className="flex justify-center items-center min-h-screen">Under Dev Switch to Mobile mode</div>
         </>
       )}
     </div>
